772fc8a69c332215b39fc7c69e747bac04bdea30
Furkan Sütcü <furkan.suetcue@alumni.fh-aachen.de>
2023-06-05 21:01:56 +0200

    main.dart erweiert auf pdfview, AndroidManifest.xml, build.gradle und pubspec.yaml angepasst für mein Smartphone

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
-import 'presenter.dart';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'audience.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path/path.dart' as path;
 
 void main() {
   runApp(MaterialApp(
@@ -31,7 +33,8 @@ class MyApp extends StatelessWidget {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => PresenterPage()),
+                            builder: (context) => PresenterPage(),
+                          ),
                         );
                       },
                       child: Text('Presenter'),
@@ -42,7 +45,8 @@ class MyApp extends StatelessWidget {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => AudiencePage()),
+                            builder: (context) => AudiencePage(),
+                          ),
                         );
                       },
                       child: Text('Audience'),
@@ -74,6 +78,203 @@ class MyApp extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class PresenterPage extends StatefulWidget {
+  @override
+  State<PresenterPage> createState() => _PresenterPageState();
+}
+
+class _PresenterPageState extends State<PresenterPage> {
+  late File? selectedFile;
+  late String chosenFile = '';
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Presenter Page'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    pickFile();
+                  },
+                  child: Text('Choose a file'),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'You chose: ${chosenFile.isNotEmpty ? chosenFile : 'No file selected'}\n',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (selectedFile != null) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PresentationPage(file: selectedFile!),
+                        ),
+                      );
+                    } else {
+                      // Handle case when no file is selected
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('No File Selected'),
+                            content: Text('Please choose a file before starting the presentation.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: Text('Start Presentation'),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: FractionalOffset.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0),
+              child: Text(
+                'Support',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+          Align(
+            alignment: FractionalOffset.bottomRight,
+            child: Container(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0),
+              child: Text(
+                'Version 1.0',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void pickFile() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+    if (result != null) {
+      setState(() {
+        selectedFile = File(result.files.single.path!);
+        chosenFile = path.basename(result.files.single.path!);
+      });
+    }
+  }
+}
+
+class AudiencePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Audience Page'),
+      ),
+      body: Center(
+        child: Text('You are logged in as an audience.'),
+      ),
+    );
+  }
+}
+
+class PresentationPage extends StatefulWidget {
+  final File file;
+
+  PresentationPage({required this.file});
+
+  @override
+  _PresentationPageState createState() => _PresentationPageState();
+}
+
+class _PresentationPageState extends State<PresentationPage> {
+  late PDFViewController _pdfController;
+  int? _currentPage = 0;
+  int? _totalPages = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Presentation Page'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PDFView(
+              filePath: widget.file.path,
+              onViewCreated: (PDFViewController pdfController) {
+                _pdfController = pdfController;
+                _pdfController.getPageCount().then((count) {
+                  setState(() {
+                    _totalPages = count;
+                  });
+                });
+              },
+              onPageChanged: (int? page, int? total) {
+                setState(() {
+                  _currentPage = page;
+                  _totalPages = total;
+                });
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  if (_pdfController != null && _currentPage! > 0) {
+                    _pdfController.setPage(_currentPage! - 1);
+                  }
+                },
+                icon: Icon(Icons.arrow_back),
+              ),
+              IconButton(
+                onPressed: () {
+                  if (_pdfController != null && _currentPage! < _totalPages! - 1) {
+                    _pdfController.setPage(_currentPage! + 1);
+                  }
+                },
+                icon: Icon(Icons.arrow_forward),
+              ),
+            ],
+          ),
+          IconButton(
+            onPressed: () {
+              // Navigate back to the main menu
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.pause),
+          ),
+        ],
+      ),
     );
   }
 }
